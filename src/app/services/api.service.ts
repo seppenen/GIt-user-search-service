24c044d0ecfaae9ef8ae4c403da637215268537f
Aleksandr Seppenen <alex.seppenen@gmail.com>
2022-01-18 23:21:22 +0100

    Code clean

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -11,12 +11,13 @@ export class ApiService  {
 
   constructor(private http: HttpClient) { }
 
-  fetch(searchParam, input, page?): Promise<any>{
+  fetch(searchParam, input, page = 1): Promise<any>{
     let params = new HttpParams()
       .set('q', `${searchParam}:${input}`)
-      .set('page', `${page}` ? `${page}`:`1`)
-      .set('per_page', '15');
-        return this.http.get(this.baseUrl, {params}).toPromise();
+      .set('page', `${page}`)
+      .set('per_page', '15')
+
+    return this.http.get(this.baseUrl, {params}).toPromise();
   }
 }
 
